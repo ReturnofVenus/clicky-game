64c462a1bde9dea74e8afb71e17a388a8dd6a99c
Ariana Romero <arianaromero143@gmail.com>
2019-03-09 23:35:14 -0700

    final changes

---
+++ src/components/Scoreboard/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./scoreboard.css";
+
+function Scoreboard(props) {
+  return (
+    <header className="scoreB">
+      <div className="row">
+        <div className="col-md-12 col-right">
+        <h1>{props.title}</h1>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2 col-right"><h6>High Score: {props.score}</h6></div>
+        <div className="col-md-2 col-right"><h6>Current Score: {props.topScore}</h6></div>
+      </div>
+
+      <div className="row">
+      <div className="col-md-12 col-right">
+      <h6>Click on a picture, but don't click on it more than once!</h6></div>
+      </div>
+    </header>
+  );
+}
+export default Scoreboard;
