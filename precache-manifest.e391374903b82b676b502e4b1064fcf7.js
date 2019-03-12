97f45abc1b29763f60d9492cfacb2e8fdeee4032
Ariana Romero <arianaromero143@gmail.com>
2019-03-11 21:26:28 -0600

    Updates

---
+++ precache-manifest.e391374903b82b676b502e4b1064fcf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "a6f1650e579f7aca1d64",
+    "url": "/clicky-game/static/js/main.22386821.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clicky-game/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "a6f1650e579f7aca1d64",
+    "url": "/clicky-game/static/css/main.54fef2e6.chunk.css"
+  },
+  {
+    "revision": "e523d430cc12f245f06319547c24f0b2",
+    "url": "/clicky-game/index.html"
+  }
+];
